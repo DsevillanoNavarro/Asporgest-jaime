0c698f3b07391cd8738b4b8cb8fd159381ef65bc
akkabarba <xjbarace307@ieshnosmachado.org>
2025-05-27 18:19:52 +0200

    cambios para probar

--- build_react.sh
+++ build_react.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-echo "🛠️  Generando build de React..."
-cd frontend || exit
-npm run build || exit
-
-echo "📂 Copiando archivos a Django..."
-rm -rf ../static/*
-# NO BORRAR NI MOVER templates/index.html
-
-cp -r build/static/* ../static/
-# NO copiar index.html
-
-echo "✅ React copiado a Django correctamente."
+cd frontend
+npm install
+npm run build
