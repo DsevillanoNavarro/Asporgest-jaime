f47bf4dde720a508df04b85ae4fcb090f57347dd
sevillanonavarro <sevillanonavarro@serviceplanet.nl>
2025-05-28 15:11:20 +0200

    First

--- ASPROGEST/settings.py
+++ ASPROGEST/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-@(tooz4f@fb@tt#e*du^uilx%pb%7#%cjzsx9dz95i2&u4w%e_")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'asporgest-jaime.onrender.com']
