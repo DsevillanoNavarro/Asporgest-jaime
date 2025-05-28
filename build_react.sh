6a3d3b9bd8b6a6aa73a2eddc8828b8313f97832d
akkabarba <xjbarace307@ieshnosmachado.org>
2025-05-28 12:06:42 +0200

    uno mas

--- build_react.sh
+++ build_react.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo "📦 Instalando dependencias..."
+pip install -r requirements.txt
+
 echo "⚛️ Compilando frontend..."
 cd frontend
 npm install
 npm run build
 cd ..
+
+echo "📁 Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
